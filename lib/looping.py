35014ea799df94c6c1fa8ef990c4d1aa5108623d
Sundeep <sundeep.kaler@gmail.com>
2024-09-07 21:43:33 -0400

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    int_squares = list()
+    for num in int_list:
+        int_square = num * num
+        int_squares.append(int_square)
+    return int_squares
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1,101):
+        
+        if (i % 15) == 0:
+            print('FizzBuzz')
+        elif (i % 3) == 0:
+            print('Fizz')
+        elif (i % 5) == 0:
+            print('Buzz')
+        else:
+            print(i)
     pass
